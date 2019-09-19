0744a79f2e1189f64ba40f4511c4a82ab90bda7d
Nitish Bhushan <nitishbhushan2013@gmail.com>
2019-09-20 00:21:13 +1000

    schema definition and consumption

--- projects/demo/server/app.js
+++ projects/demo/server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 const app = express();
 const port =9002;
 
@@ -7,6 +8,8 @@ const port =9002;
 // setup middleware for the graphql entry point
 app.use('/graphql', graphqlHTTP({
     // contain the graphql schema 
+    //schema : schema
+    schema //ES6 convention
 }))
 app.listen(port, ()=>{
     console.log("server is listening at port "+port);
--- projects/demo/server/schema/schema.js
+++ projects/demo/server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
-const {GraphQLObjectType, GraphQLString} = graphql; // dereferencing 
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql; //ES6: dereferencing, get properties from graphql
 
+
+/*
+This schema will hold three important steps 
+ 1. type definition of different objects 
+ 2. relationship definition between these objects
+ 3. RootQuery definition : These will acts as a entry point to the schema 
+
+*/
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields : ()=>({
@@ -9,3 +17,27 @@ const BookType = new GraphQLObjectType({
         genre : {type: GraphQLString}
     })
 });
+
+//define the rootQuery
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields : {
+        /*define all the entries or queries that a client can perform. 
+        client query would be 
+        book{
+            title
+        }
+        */
+        book:{
+            type:BookType,
+            args:{id:{type:GraphQLString}},
+            resolve(parent,args){
+                // code to get data from the server
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
