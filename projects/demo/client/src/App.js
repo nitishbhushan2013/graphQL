65354117ab4be10ce4a576db53ec5b6d4fb9c64a
Nitish Bhushan <nitishbhushan2013@gmail.com>
2019-09-20 22:22:13 +1000

    UI React stateless component: BookList

--- projects/demo/client/src/App.js
+++ projects/demo/client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import BookList from './components/BookList/BookList';
 
 function App() {
   return (
     <div className="App">
-      <h1> Ready for rock and roll </h1>
+      <h1> GraphQL DEMO Project : Reading List</h1>
+      <BookList/>
     </div>
   );
 }
---
+++ projects/demo/client/src/components/BookList/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const bookList = (props) => {
+    return(
+        <div>
+            <ui id="book-list">
+                <li> Dummy Book - 1</li>
+            </ui>
+        </div>
+       
+    );
+}
+export default bookList;
